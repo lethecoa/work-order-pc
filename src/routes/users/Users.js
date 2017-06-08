f529747a029db248f1a21e97a5db7a7512b70b66
yanglb <yanglibai0425@sina.cn>
2017-06-08 10:28:25 +0800

    test

--- src/routes/users/Users.js
+++ src/routes/users/Users.js
@@ -4,12 +4,13 @@ import styles from './Users.less';
 import UsersComponent from '../../components/users/Users';
 import MainLayout from '../../components/layout/MainLayout';
 
-//....
+//....user
 function Users({ location }) {
   return (
     <MainLayout location={location}>
       <div className={styles.normal}>
         <UsersComponent />
+        <h1>user</h1>
       </div>
     </MainLayout>
   );
