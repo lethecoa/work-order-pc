1507d2e94433445c58faae3db718188ab36f8ca0
yanglb <yanglibai0425@sina.cn>
2017-06-12 21:27:05 +0800

    orderStep

---
+++ src/components/OrderStep/OrderStep.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './OrderStep.css';
+import {Steps} from 'antd';
+
+const Step = Steps.Step;
+
+function OrderStep( props ) {
+	return (
+		<div className={styles.normal}>
+			<Steps className={styles.step} size="small" current={props.currentStep}>
+				<Step title="填写委托单"/>
+				<Step title="确认填写信息"/>
+				<Step title="等待客服处理"/>
+			</Steps>
+		</div>
+	);
+}
+
+export default OrderStep;
