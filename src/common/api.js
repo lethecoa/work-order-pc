29cbaa63195a3dd97a3f17edf0df7924158ea42c
lethecoa <lethecoa@163.com>
2017-06-13 15:31:37 +0800

    api update

--- src/common/api.js
+++ src/common/api.js
@@ -2,5 +2,6 @@ module.exports = {
 	userLogin: '/api/wo/login',
 	userLogout: '',
 	getItemPriceById: '/api/workorder/getItemPriceById',
-	saveSign: '/api/workorder/saveSign'
+	saveSign: '/api/workorder/saveSign',
+	uploadExcel: '/callCenter/resolveExcel',
 }
