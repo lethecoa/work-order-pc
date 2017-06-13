d3b150475609a01927b8af0186e5e53a08eedfde
lethecoa <lethecoa@163.com>
2017-06-13 14:09:13 +0800

    配置更新了

--- src/common/config.js
+++ src/common/config.js
@@ -1,19 +1,23 @@
 module.exports = {
-  debug: true,
-  PAGE_SIZE: 3,
-  name: '客服工单系统',
-  storage_prefix: 'wop$',
-  local: {
-    user: 'user',
-    loginInfo: 'loginInfo'
-  },
-	materialOptions : [
+	debug: true,
+	PAGE_SIZE: 3,
+	name: '客服工单系统',
+	storage_prefix: 'wop$',
+	local: {
+		user: 'user',
+		loginInfo: 'loginInfo'
+	},
+	materialOptions: [
 		{ label: '身份证', value: 1 },
 		{ label: '社保卡', value: 2 },
 		{ label: '不携带任何材料', value: 0 },
 	],
-	formItemLayout : {
+	formItemLayout: {
 		labelCol: { span: 4 },
 		wrapperCol: { span: 12 }
+	},
+	ritField: {
+		name: 'name', sex: 'sex', birthday: 'birthday', tel: 'tel',
+		disease: 'disease', cardDate: 'cardDate', drugs: 'drugs'
 	}
 }
--- src/common/modular.js
+++ src/common/modular.js
@@ -1,6 +1,15 @@
+import config from './config';
+
 const root = '/';
 const confirm = 'confirm';
 
+/**
+ * url：路由对应的访问路径
+ * nane：模块英文名称
+ * cn：模块中文名称
+ * ritRef：居民信息表对应的服务端名称
+ * ritDoctor：居民信息表里医生显示的字段配置
+ */
 module.exports = {
   /**
    * 首页
@@ -10,18 +19,24 @@ module.exports = {
 	/**
    * 签约家庭
 	 */
-	signFamily: { url: 'signFamily', name: 'SignFamily', cn: '签约家庭' },
+  signFamily: {
+    url: 'signFamily', name: 'SignFamily', cn: '签约家庭', ritRef: 'dljmqy',
+    ritDoctor: [ config.ritField.name, config.ritField.sex, config.ritField.birthday, config.ritField.tel ]
+  },
 	/**
    * 预约代理
 	 */
-	residentSign: 'residentSign',
+  residentSign: 'residentSign',
   residentInspect: 'residentInspect',
   newborn: 'newborn',
   postpartum: 'postpartum',
   /**
    * 慢性病随访通知
    */
-  chronicDisease: { url: 'chronicDisease', name: 'ChronicDisease', cn: '慢性病随访通知' },
+  chronicDisease: {
+    url: 'chronicDisease', name: 'ChronicDisease', cn: '慢性病随访通知', ritRef: 'mbsftz',
+    ritDoctor: [ config.ritField.name, config.ritField.sex, config.ritField.birthday, config.ritField.tel ]
+  },
   newestPolicy: 'newestPolicy',
   newestActivity: 'newestActivity',
   antenatalCare: 'antenatalCare',
