9e28812136247777d84fe71bdebf6ef63fac2d65
yanglb <yanglibai0425@sina.cn>
2017-06-18 16:31:32 +0800

    提交成功提示信息

--- src/common/config.js
+++ src/common/config.js
@@ -16,10 +16,12 @@ module.exports = {
 	userType: { doctor: 'doctor', worker: 'worker' },
 	/** “居民信息控件”单元格的编辑模式 */
 	ritStatus: { general: '显示状态', editing: '编辑中', cancel: '取消编辑' },
-	/** 根据委托单名称定义“居民信息控件”表头的显示项	*/
+	/** 根据委托单名称定义“居民信息控件”表头的显示项  */
 	ritField: {
 		name: 'name', sex: 'sex', birthday: 'birthday', tel: 'tel', present: 'isOnScene', visit: 'isInterview',
 		disease: 'relatedDiseases', diseaseCase: 'diseaseSituation', cardDate: 'cardDate', drugs: 'drugs',
 		notice: 'notice', operation: 'operation', booking: 'booking', remark: 'remark', followUp: 'followUp'
-	}
-}
+	},
+	/** 提交成功提示信息  */
+	SUCCESS: '提交成功'
+};
