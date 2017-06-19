dcc09f55ef3ff8b41ca60be3fa28a413907cda18
yanglb <yanglibai0425@sina.cn>
2017-06-19 11:10:54 +0800

    modular

--- src/common/modular.js
+++ src/common/modular.js
@@ -1,5 +1,6 @@
 import config from './config';
-
+import action from './action';
+import model from './model';
 const root = '/';
 
 /**
@@ -23,7 +24,7 @@ module.exports = {
 		url: 'signFamily', name: 'signFamily', cn: '签约家庭', ritRef: 'dljmqy',
 		ritDoctor: [ config.ritField.name, config.ritField.sex, config.ritField.birthday, config.ritField.tel ],
 		ritWorker: [ config.ritField.name, config.ritField.sex, config.ritField.birthday, config.ritField.tel,
-		config.ritField.present, config.ritField.booking, config.ritField.operation ]
+			config.ritField.present, config.ritField.booking, config.ritField.operation ]
 	},
 	/**
 	 * 预约代理
@@ -52,7 +53,7 @@ module.exports = {
 		tpl: config.tpl + 'chronicDisease.xls',
 		ritDoctor: [ config.ritField.name, config.ritField.sex, config.ritField.birthday, config.ritField.tel ],
 		ritWorker: [ config.ritField.name, config.ritField.sex, config.ritField.birthday, config.ritField.tel,
-		config.ritField.present, config.ritField.followUp, config.ritField.remark, config.ritField.operation ]
+			config.ritField.present, config.ritField.followUp, config.ritField.remark, config.ritField.operation ]
 	},
 	newestPolicy: {
 		url: 'newestPolicy', name: 'newestPolicy', cn: '最新政策通知', ritRef: 'zxzctz',
@@ -62,7 +63,10 @@ module.exports = {
 	},
 	newestActivity: {
 		url: 'newestActivity', name: 'newestActivity', cn: '最新活动通知', ritRef: 'zxhdtz',
-		ritDoctor: [ config.ritField.name, config.ritField.sex, config.ritField.birthday, config.ritField.tel ]
+		model: model.noticeAgent, action: action.NA_savePolicy,
+		ritDoctor: [ config.ritField.name, config.ritField.sex, config.ritField.birthday, config.ritField.tel ],
+		ritWorker: [ config.ritField.name, config.ritField.sex, config.ritField.birthday, config.ritField.tel,
+			config.ritField.present, config.ritField.remark, config.ritField.operation ]
 	},
 	antenatalCare: {
 		url: 'antenatalCare', name: 'antenatalCare', cn: '孕产妇产检通知', ritRef: 'yfcjtz',
