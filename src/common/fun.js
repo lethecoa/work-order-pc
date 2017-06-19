69f8273af5d4893fdd49730ebb9ba07434c55d61
yanglb <yanglibai0425@sina.cn>
2017-06-19 17:38:25 +0800

    fun

--- src/common/fun.js
+++ src/common/fun.js
@@ -2,37 +2,45 @@ import moment from 'moment';
 import config from './config';
 
 module.exports = {
-  /**
-   * 包装系统的console，指定force=true可以强制在非Debug模式下打印信息
-   * 
-   * @param {any} obj 要打印的数据
-   * @param {string} [mark='未定义标签'] 提示标签
-   * @param {string} [moudle='none'] 模块名称
-   * @param {boolean} [force=false] 强制输出此信息
-   */
-  print: ( obj, mark = '未定义标签', moudle = 'none', force = false ) => {
-    if ( config.debug || force ) {
-      console.log( moudle + ' >>> ' + mark + ': ', obj );
-    }
-  },
-  /**
-   * 打印模块的加载时间
-   * 
-   * @param {string} moudle 模块名称
-   */
-  printLoader: ( moudle ) => {
-    if ( config.debug ) {
-      console.log( '============== 加载（' + moment().format( 'MM-DD HH:mm:ss' ) + '）：' + moudle + ' ==============' );
-    }
-  },
-  /**
-   * 将model和action组合成路径形式：model/action
-   * 
-   * @param {string} model 
-   * @param {string} action 
-   * @returns 
-   */
-  fuse: ( model, action ) => {
-    return model + '/' + action;
-  }
-}
+	/**
+	 * 包装系统的console，指定force=true可以强制在非Debug模式下打印信息
+	 *
+	 * @param {any} obj 要打印的数据
+	 * @param {string} [mark='未定义标签'] 提示标签
+	 * @param {string} [moudle='none'] 模块名称
+	 * @param {boolean} [force=false] 强制输出此信息
+	 */
+	print: ( obj, mark = '未定义标签', moudle = 'none', force = false ) => {
+		if ( config.debug || force ) {
+			console.log( moudle + ' >>> ' + mark + ': ', obj );
+		}
+	},
+	/**
+	 * 打印模块的加载时间
+	 *
+	 * @param {string} moudle 模块名称
+	 */
+	printLoader: ( moudle ) => {
+		if ( config.debug ) {
+			console.log( '============== 加载（' + moment().format( 'MM-DD HH:mm:ss' ) + '）：' + moudle + ' ==============' );
+		}
+	},
+	/**
+	 * 将model和action组合成路径形式：model/action
+	 *
+	 * @param {string} model
+	 * @param {string} action
+	 * @returns
+	 */
+	fuse: ( model, action ) => {
+		return model + '/' + action;
+	},
+	/**
+	 * 将时间毫秒值转为字符串形式
+	 * @param nS
+	 * @returns {string}
+	 */
+	getLocalTime: ( nS ) => {
+		return new Date( parseInt( nS ) ).toLocaleString().replace( /:\d{1,2}$/, ' ' );
+	}
+};
