dc455feb0bb9ae4e39b1e383e7b71d0dac7083ec
yanglb <yanglibai0425@sina.cn>
2017-06-20 00:22:45 +0800

    api

--- src/common/api.js
+++ src/common/api.js
@@ -4,14 +4,30 @@ module.exports = {
 	userLogin: config.api + '/wo/login',
 	userLogout: '',
 	getItemInfoById: config.api + '/workorder/getItemInfoById',
-	saveSign: config.api + '/workorder/saveSign',
 	uploadExcel: config.api + '/callCenter/resolveExcel',
+	/** 保存预约居民签约 */
+	saveSign: config.api + '/workorder/saveSign',
+	/** 保存预约居民体检 */
 	savePhysicalExam: config.api + '/workorder/savePhysicalExam',
+	/** 保存预约新生儿家庭访视 */
 	saveNewBorn: config.api + '/workorder/saveNewBorn',
+	/** 保存预约产后访视 */
 	savePostpartum: config.api + '/workorder/savePostpartum',
+	/** 保存慢病随访通知 */
+	saveChronic: config.api + '/workorder/saveChronic',
+	/** 保存最新政策通知 */
 	savePolicy: config.api + '/workorder/savePolicy',
-	/** 保存最新活动通知委托单 */
+	/** 保存最新活动通知 */
 	saveActivity: config.api + '/workorder/saveActivity',
+	/** 保存孕产妇产检通知 */
+	saveGravida: config.api + '/workorder/saveGravida',
+	/** 保存儿童健康随访通知 */
+	saveChildren: config.api + '/workorder/saveChildren',
+	/** 保存用药提醒 */
+	saveDrug: config.api + '/workorder/saveDrug',
+	/** 保存用药疗效跟踪 */
+	saveDrugeffect: config.api + '/workorder/saveDrugeffect',
+	/** 查询委托单列表 */
 	getOrders: config.api + '/callCenter/getOrders',
 	/** 查询委托单明细 */
 	getOrderDetail: config.api + '/callCenter/getOrderDetail',
