2298dae20fbb285e8f74cc0e21092199e0baa218
yanglb <yanglibai0425@sina.cn>
2017-06-20 00:31:05 +0800

    model

--- src/models/noticeAgentModel.js
+++ src/models/noticeAgentModel.js
@@ -1,11 +1,17 @@
 import {fun, model, action} from '../common';
-import {savePolicy,saveActivity} from '../services/noticAgentService';
+import {saveChronic, savePolicy, saveActivity, saveGravida, saveChildren} from '../services/noticAgentService';
 
 export default {
 	namespace: model.noticeAgent,
 	state: {},
 	reducers: {},
 	effects: {
+		*saveChronic( { payload }, { call, put } ) {
+			fun.print( payload, 'saveChronic', model.noticeAgent );
+			const data = yield call( saveChronic, payload.data );
+			payload.fun( data );
+			yield put( { type: fun.fuse( model.order, action.order_changeSubmitSate ) } );
+		},
 		*savePolicy( { payload }, { call, put } ) {
 			fun.print( payload, 'savePolicy', model.noticeAgent );
 			const data = yield call( savePolicy, payload.data );
@@ -15,6 +21,18 @@ export default {
 		*saveActivity( { payload }, { call, put } ) {
 			fun.print( payload, 'saveActivity', model.noticeAgent );
 			const data = yield call( saveActivity, payload.data );
+			payload.fun( data );
+			yield put( { type: fun.fuse( model.order, action.order_changeSubmitSate ) } );
+		},
+		*saveGravida( { payload }, { call, put } ) {
+			fun.print( payload, 'saveGravida', model.noticeAgent );
+			const data = yield call( saveGravida, payload.data );
+			payload.fun( data );
+			yield put( { type: fun.fuse( model.order, action.order_changeSubmitSate ) } );
+		},
+		*saveChildren( { payload }, { call, put } ) {
+			fun.print( payload, 'saveChildren', model.noticeAgent );
+			const data = yield call( saveChildren, payload.data );
 			payload.fun( data );
 			yield put( { type: fun.fuse( model.order, action.order_changeSubmitSate ) } );
 		},
--- src/models/trackingReminderModel.js
+++ src/models/trackingReminderModel.js
@@ -1,8 +1,23 @@
-import {model} from '../common';
+import {fun, model, action} from '../common';
+import {saveDrug, saveDrugeffect} from '../services/trackingReminderService';
+
 export default {
 	namespace: model.trackingReminder,
 	state: {},
 	reducers: {},
-	effects: {},
+	effects: {
+		*saveDrug( { payload }, { call, put } ) {
+			fun.print( payload, 'saveDrug', model.trackingReminder );
+			const data = yield call( saveDrug, payload.data );
+			payload.fun( data );
+			yield put( { type: fun.fuse( model.order, action.order_changeSubmitSate ) } );
+		},
+		*saveDrugeffect( { payload }, { call, put } ) {
+			fun.print( payload, 'saveDrugeffect', model.trackingReminder );
+			const data = yield call( saveDrugeffect, payload.data );
+			payload.fun( data );
+			yield put( { type: fun.fuse( model.order, action.order_changeSubmitSate ) } );
+		},
+	},
 	subscriptions: {},
 };
