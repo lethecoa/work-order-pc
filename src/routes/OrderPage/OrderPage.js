6b342799e819d542d1da733a705fb2fd85549681
yanglb <yanglibai0425@sina.cn>
2017-06-21 11:01:15 +0800

    orderPage

--- src/routes/OrderPage/OrderPage.js
+++ src/routes/OrderPage/OrderPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { connect } from 'dva';
-import { routerRedux } from 'dva/router';
-import { Spin, Row, Col, Form, Button, notification, Modal, Radio } from 'antd';
-import { action, model, fun, config, modular } from '../../common';
+import {connect} from 'dva';
+import {routerRedux} from 'dva/router';
+import {Spin, Row, Col, Form, Button, notification, Modal, Radio} from 'antd';
+import {action, model, fun, config, modular} from '../../common';
 import {
 	OrderStep,
 	BaseInfo,
@@ -25,7 +25,7 @@ import styles from './OrderPage.less';
 const moduleName = 'orderPage';
 const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
-
+const { present, remark, visit } = config.ritField;
 let residentInfoTable;
 let baseInfo;
 let payModal;
@@ -45,6 +45,8 @@ const OrderPage = ( {
 	const { currentData, currentStep, display, disabled, displayConfirm, submitDisabled, displayBack, displayNew } = orderModel;
 	const { orgName, remainingBalance } = currentData;
 	const { pagination, serviceDetail, residentList } = workerModel ? workerModel : {};
+
+	/** 获取页面path,初始化dataList */
 	let path;
 	if ( userType === config.userType.worker ) {
 		path = route.path.substr( route.path.indexOf( '/' ) + 1 );
@@ -63,9 +65,9 @@ const OrderPage = ( {
 	};
 	/** 医生端-校验表单输入数据*/
 	const validAndConfirm = () => {
-		baseInfo.validateFieldsAndScroll(( errOut ) => {
+		baseInfo.validateFieldsAndScroll( ( errOut ) => {
 			if ( !errOut ) {
-				need.validateFieldsAndScroll(( err ) => {
+				need.validateFieldsAndScroll( ( err ) => {
 					if ( !err ) {
 						dataList = residentInfoTable.getData();
 						if ( dataList === undefined || dataList.length === 0 ) {
@@ -91,9 +93,9 @@ const OrderPage = ( {
 	};
 	/** 医生端-提交表单数据 */
 	const handleSubmit = ( expenseAccount ) => {
-		baseInfo.validateFieldsAndScroll(( errOut, valuesOut ) => {
+		baseInfo.validateFieldsAndScroll( ( errOut, valuesOut ) => {
 			if ( !errOut ) {
-				need.validateFieldsAndScroll(( err, values ) => {
+				need.validateFieldsAndScroll( ( err, values ) => {
 					if ( !err ) {
 						let result = {
 							data: Object.assign( values, valuesOut, { expenseAccount, residentQueryDtoList: dataList } ),
@@ -143,6 +145,29 @@ const OrderPage = ( {
 	const backToList = () => {
 		dispatch( routerRedux.push( { pathname: modular.index.url + modular.orderList.url, query: pagination, } ) );
 	};
+	/** 初始化数据，将可编辑的字段改为 object 类型 */
+	const formatData = ( data ) => {
+		if ( typeof data !== 'undefined' && data.length > 0 ) {
+			data.forEach( function ( item, index, arr ) {
+				let obj = arr[ index ];
+				obj.myStatus = config.ritStatus.general;
+				// 监听数量，代表每一列有多少个可编辑单元格，
+				// 只有当所有单元格都触发了回调函数才会执行最终的保存或提交动作
+				obj.monitor = 0;
+
+				if ( !obj.hasOwnProperty( remark.key ) ) {
+					obj[ remark.key ] = '';
+				}
+				if ( !obj.hasOwnProperty( present.key ) ) {
+					obj[ present.key ] = '0';
+				}
+				if ( !obj.hasOwnProperty( visit.key ) ) {
+					obj[ visit.key ] = '0';
+				}
+			} );
+		}
+		return data;
+	};
 	/** 显示提交结果 */
 	const openNotificationWithIcon = ( data, index ) => {
 		payModal.handleOver();
@@ -179,7 +204,7 @@ const OrderPage = ( {
 				{userType === config.userType.doctor ?
 					<div>
 						<div className={styles.header}>{modular[ path ].cn + '委托表'}</div>
-						<OrderStep currentStep={currentStep} />
+						<OrderStep currentStep={currentStep}/>
 					</div>
 					:
 					<div className={styles.center}>
@@ -191,41 +216,41 @@ const OrderPage = ( {
 					</div>
 				}
 				<Form onSubmit={showModal}>
-					<BaseInfo {...baseInfoProps} ref={e => ( baseInfo = e )} />
-					{path === 'signFamily' ? <ResidentSign disabled={disabled} ref={e => ( need = e )} signSite={orgName} /> : ''}
-					{path === 'residentSign' ? <ResidentSign disabled={disabled} ref={e => ( need = e )} signSite={orgName} /> : ''}
+					<BaseInfo {...baseInfoProps} ref={e => ( baseInfo = e )}/>
+					{path === 'signFamily' ? <ResidentSign disabled={disabled} ref={e => ( need = e )} signSite={orgName}/> : ''}
+					{path === 'residentSign' ? <ResidentSign disabled={disabled} ref={e => ( need = e )} signSite={orgName}/> : ''}
 					{path === 'workeryyjmqy' ? <ResidentSign disabled={disabled} {...serviceDetail} /> : ''}
-					{path === 'residentInspect' ? <ResidentInspect disabled={disabled} ref={e => ( need = e )} examineSite={orgName} /> : ''}
+					{path === 'residentInspect' ? <ResidentInspect disabled={disabled} ref={e => ( need = e )} examineSite={orgName}/> : ''}
 					{path === 'workeryyjmtj' ? <ResidentInspect disabled={disabled} {...serviceDetail} /> : ''}
-					{path === 'newborn' ? <Newborn disabled={disabled} ref={e => ( need = e )} /> : ''}
+					{path === 'newborn' ? <Newborn disabled={disabled} ref={e => ( need = e )}/> : ''}
 					{path === 'workeryyxsfs' ? <Newborn disabled={disabled} {...serviceDetail} /> : ''}
-					{path === 'postpartum' ? <Newborn disabled={disabled} ref={e => ( need = e )} /> : ''}
+					{path === 'postpartum' ? <Newborn disabled={disabled} ref={e => ( need = e )}/> : ''}
 					{path === 'workeryychfs' ? <Newborn disabled={disabled} {...serviceDetail} /> : ''}
 
-					{path === 'chronicDisease' ? <ChronicDisease disabled={disabled} ref={e => ( need = e )} interviewSite={orgName} /> : ''}
+					{path === 'chronicDisease' ? <ChronicDisease disabled={disabled} ref={e => ( need = e )} interviewSite={orgName}/> : ''}
 					{path === 'workermbsftz' ? <ChronicDisease disabled={disabled} {...serviceDetail} /> : ''}
-					{path === 'newestPolicy' ? <NewestPolicy disabled={disabled} ref={e => ( need = e )} /> : ''}
+					{path === 'newestPolicy' ? <NewestPolicy disabled={disabled} ref={e => ( need = e )}/> : ''}
 					{path === 'workerzxzctz' ? <NewestPolicy disabled={disabled} {...serviceDetail} /> : ''}
-					{path === 'newestActivity' ? <NewestActivity disabled={disabled} ref={e => ( need = e )} /> : ''}
+					{path === 'newestActivity' ? <NewestActivity disabled={disabled} ref={e => ( need = e )}/> : ''}
 					{path === 'workerzxhdtz' ? <NewestActivity disabled={disabled} {...serviceDetail} /> : ''}
-					{path === 'antenatalCare' ? <AntenatalCare disabled={disabled} ref={e => ( need = e )} antenatalCareSite={orgName} /> : ''}
+					{path === 'antenatalCare' ? <AntenatalCare disabled={disabled} ref={e => ( need = e )} antenatalCareSite={orgName}/> : ''}
 					{path === 'workeryfcjtz' ? <AntenatalCare disabled={disabled} {...serviceDetail} /> : ''}
-					{path === 'childHealth' ? <ChildHealth disabled={disabled} ref={e => ( need = e )} interviewSite={orgName} /> : ''}
+					{path === 'childHealth' ? <ChildHealth disabled={disabled} ref={e => ( need = e )} interviewSite={orgName}/> : ''}
 					{path === 'workeretsftz' ? <ChildHealth disabled={disabled} {...serviceDetail} /> : ''}
 
-					{path === 'medication' ? <Medication disabled={disabled} ref={e => ( need = e )} /> : ''}
+					{path === 'medication' ? <Medication disabled={disabled} ref={e => ( need = e )}/> : ''}
 					{path === 'workeryytx00' ? <Medication disabled={disabled} {...serviceDetail} /> : ''}
-					{path === 'curativeEffect' ? <Medication disabled={disabled} ref={e => ( need = e )} /> : ''}
+					{path === 'curativeEffect' ? <Medication disabled={disabled} ref={e => ( need = e )}/> : ''}
 					{path === 'workeryylxgz' ? <Medication disabled={disabled} {...serviceDetail} /> : ''}
 
-					{path === 'hypertension' ? <Hypertension disabled={disabled} ref={e => ( need = e )} /> : ''}
+					{path === 'hypertension' ? <Hypertension disabled={disabled} ref={e => ( need = e )}/> : ''}
 					{path === 'workergxysf0' ? <Hypertension disabled={disabled} {...serviceDetail} /> : ''}
-					{path === 'diabetes' ? <Diabetes disabled={disabled} ref={e => ( need = e )} /> : ''}
+					{path === 'diabetes' ? <Diabetes disabled={disabled} ref={e => ( need = e )}/> : ''}
 					{path === 'workertnbsf0' ? <Diabetes disabled={disabled} {...serviceDetail} /> : ''}
 
 					<ResidentInfoTable name={modular[ path ].name} userType={userType} monitor={modular[ path ].monitor}
-						data={dataList} onSave={( i, r ) => saveRow( i, r )} onSubmit={( i, r ) => submitRow( i, r )}
-						disabled={disabled} ref={e => ( residentInfoTable = e )} />
+					                   data={formatData( dataList )} onSave={( i, r ) => saveRow( i, r )} onSubmit={( i, r ) => submitRow( i, r )}
+					                   disabled={disabled} ref={e => ( residentInfoTable = e )}/>
 					{userType === config.userType.doctor ?
 						<div>
 							<div className={styles.submit} style={{ display: displayConfirm }}>
@@ -235,12 +260,12 @@ const OrderPage = ( {
 								<Row>
 									<Col span={12}>
 										<Button size="large" type="primary" htmlType="submit" style={{ width: 200 }} disabled={submitDisabled}
-											loading={loading}>确认委托并支付</Button>
+										        loading={loading}>确认委托并支付</Button>
 									</Col>
 									<Col span={12}>
 										<Button size="large" type="primary" style={{ width: 200, display: displayBack }} onClick={validAndConfirm}>返回修改</Button>
 										<Button size="large" type="primary" style={{ width: 200, display: displayNew }}
-											onClick={function () { location.reload() }}>新建委托单</Button>
+										        onClick={function () { location.reload() }}>新建委托单</Button>
 									</Col>
 								</Row>
 							</div>
